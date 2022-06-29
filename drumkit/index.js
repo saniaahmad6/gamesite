5c0d1a2524768d99c6416b882fc8255f671de7c0
Sania Ahmad <sania.ahmad483@gmail.com>
2022-06-29 16:25:59 +0530

    js objects

--- drumkit/index.js
+++ drumkit/index.js
@@ -1,1 +1,10 @@
-function 
+for (var i=0;i<document.querySelectorAll("button.drum").length;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function () {
+        this.style.color="white";
+        });
+}
+
+
+
+// var audio_obj=new Audio("sounds/tom-1.mp3");
+//     audio_obj.play();
